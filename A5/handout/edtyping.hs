9fe742498bfe18f54d852ead64688daa48e4e58f
Santiago <scuellar@princeton.edu>
2014-04-24 23:10:35 -0400

    small fix to edtyping

--- A5/handout/edtyping.hs
+++ A5/handout/edtyping.hs
@@ -9,8 +9,8 @@ check_wf :: Map.Map String () -> Exp -> Maybe ()
 -- Map String to unit, if it's there, it's in scope
 -- typed and untyped functions are both well-formed under the same conditions
 -- the type annotations aren't special
-check_wf g (Int i) = Just ()
-check_wf g (Bool b) = Just ()
+check_wf _ (Int i) = Just ()
+check_wf _ (Bool b) = Just ()
 check_wf g (If e1 e2 e3) = case (check_wf g e1, check_wf g e2, check_wf g e3) of
                                 (Just (), Just (), Just()) -> Just ()
                                 _ -> Nothing
@@ -27,14 +27,14 @@ check_wf g (PrimOp Times [e1, e2]) = case (check_wf g e1, check_wf g e2) of
                                             (Just (), Just ()) -> Just ()
                                             _ -> Nothing
 check_wf g (PrimOp Negate [e1]) = check_wf g e1
-check_wf g (PrimOp _ _) = Nothing
-check_wf g (Fun s1 s2 t1 t2 e) = check_wf g (UTFun s1 s2 e)
+check_wf g (Fun f x tf tx e) = check_wf g (UTFun f x e)
 check_wf g (Check e t) = check_wf g e
 check_wf g (UTFun f x e) = check_wf (Map.insert x () (Map.insert f () g)) e
 check_wf g (Apply e1 e2) = case (check_wf g e1, check_wf g e2) of
                                     (Just (), Just ()) -> Just ()
                                     _ -> Nothing
 check_wf g (Var s) = Map.lookup s g
+check_wf _ _ = Nothing
 
 check_wellformed :: Exp -> Maybe ()
 check_wellformed e = check_wf Map.empty e
