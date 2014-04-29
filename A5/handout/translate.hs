e0d34e4e1f2f169cb94510ab150f92b58f228eda
Jonathan Balkind <jbalkind@cs.princeton.edu>
2014-04-28 22:51:55 -0400

    reordered fun cases in ttyping, dunno what I changed in translate

--- A5/handout/translate.hs
+++ A5/handout/translate.hs
@@ -121,7 +121,7 @@ translateExp g (D.Fun f x tx tf e) = -- I just realised - I think that the funct
           let t2 = translateType tf in
                    (if (t3 == t2)
                     then Just (M.Fun f x (translateType tx) t2 e2', M.ARROW (translateType tx) t3)
-                    else Just (M.Fun f x (translateType tx) t2 (cast t2 (tagify t3 e2')), M.ARROW (translateType tx) t2))
+                    else Just (M.Fun f x (translateType tx) t2 (cast t2 (tagify t3 e2')), M.ARROW (translateType tx) (translateType tf)))
 -- Checks
 translateExp g (D.Check e t) = 
     case translateExp g e of
