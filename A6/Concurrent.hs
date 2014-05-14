db8dea711a21b4c449a4e41e3ea6022495502048
Jonathan Balkind <jbalkind@cs.princeton.edu>
2014-05-14 19:26:33 -0400

    Concurrent change

--- A6/Concurrent.hs
+++ A6/Concurrent.hs
@@ -18,6 +18,7 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.State
 import Control.Exception
+import System.Timeout
 
 -- A version of the standard library [forkFinally] that doesn't
 -- mask asynchronous exceptions.
@@ -54,6 +55,8 @@ parallel as
       a : as' -> 
         do mvar <- fork_joinIO a
            parallel as'
-           takeMVar mvar
+           --takeMVar mvar
+           timeout 2000000 (takeMVar mvar)
+           return ()
 
 
