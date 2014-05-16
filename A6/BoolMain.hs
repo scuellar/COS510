e2d0ef235d91a92ca9929cf2f61ffb40ec489913
Elba Garza <elbagarza@elba.cs.princeton.edu>
2014-05-16 14:09:36 -0400

    fixed boolmain and finished natmain

--- A6/BoolMain.hs
+++ A6/BoolMain.hs
@@ -13,28 +13,29 @@ env1 = M.empty
 env2 = M.insert x True env1 
 env3 = M.insert y False env2 
 
-type Test = (BEnv, BoolExp)
+type Test = (BEnv, BoolExp, [Char])
 
-test1 = (env1, t)
-test2 = (env1, f)
-test3 = (env1, t :&&: (f :||: t) :&&: (Not f))
-test4 = (env2, BVar x)
-test5 = (env3, BVar x :&&: BVar y)
-test6 = (env3, BVar x :&&: (BVar y :||: (t :&&: (f :||: t)))) --True
-test7 = (env3, BVar x :&&: (BVar y :||: (t :&&: (f :||: f)))) --False
-test8 = (env3, Not (BVar y))
-test9 = (env3, Not (BVar y :&&: BVar x))
-test10 = (env3, BVar y :||: (BVar x :&&: BVar x))
+test1 = (env1, t, "True = ")
+test2 = (env1, f, "False = ")
+test3 = (env1, t :&&: (f :||: t) :&&: (Not f), "True && (False || True) && ~False = ")
+test4 = (env2, BVar x, "x = ")
+test5 = (env3, BVar y, "y = ")
+test6 = (env3, BVar x :&&: BVar y, "x && y = ")
+test7 = (env3, BVar x :&&: (BVar y :||: (t :&&: (f :||: t))), "x && (y || (True && (False || True)) = ") --True
+test8 = (env3, BVar x :&&: (BVar y :||: (t :&&: (f :||: f))), "x && (y || (True && (False || False)) = ") --False
+test9 = (env3, Not (BVar y), "~y = ")
+test10 = (env3, Not (BVar y :&&: BVar x), " ~(y && x) = ")
+test11 = (env3, BVar y :||: (BVar x :&&: BVar x), "y || (x && x) = ")
 
-tests = [test1, test2, test3, test4, test5]
-tests2 = [test6, test7, test8, test9, test10]
+tests = [test1, test2, test3, test4, test5, test6, test7, test8, test9, test10]
 
 run_tests :: [Test] -> IO ()
 run_tests ts = run_t 1 ts
   where
     run_t n []     = return ()
-    run_t n ((env,b):ts) = do
+    run_t n ((env,b,text):ts) = do
       putStr ("test " ++ show n ++ ":\n")
+      putStr (text)
       start_bool env b
       putStr "\n"
       run_t (n+1) ts
--- A6/NatMain.hs
+++ A6/NatMain.hs
@@ -22,33 +22,36 @@ env2 = M.insert zz three env1
 env3 = M.insert opo oneplusone env2
 env4 = M.insert ftt fivetimesthree env3
 
-type Test = (NEnv, NatExp)
+type Test = (NEnv, NatExp, [Char])
 
-test1  = (env1, zero)
-test2  = (env1, one)
-test3  = (env1, zero :+: zero)
-test4  = (env1, zero :+: one)
-test5  = (env1, one :+: zero)
-test6  = (env1, one :+: one)
-test7  = (env1, zero :*: zero)
-test8  = (env1, zero :*: one) 
-test9  = (env1, one :*: zero)
-test10 = (env1, one :+: (zero :*: two))
-test11 = (env1, two :*: (one :+: zero))
-test12 = (env1, two :*: (two :*: (zero :*: zero)))
-test13 = (env1, two :*: (two :+: (zero :*: zero)))
-test14 = (env2, (NVar zz))
-test15 = (env3, (NVar opo))
-test16 = (env4, (NVar ftt))
+test1  = (env1, zero, "0 = ")
+test2  = (env1, one, "1 = ")
+test3  = (env1, zero :+: zero, "0 + 0 = ")
+test4  = (env1, zero :+: one, "0 + 1 = ")
+test5  = (env1, one :+: zero, "1 + 0 = ")
+test6  = (env1, one :+: one, "1 + 1 = ")
+test7  = (env1, zero :*: zero, "0 * 0 = ")
+test8  = (env1, zero :*: one, "0 * 1 = ") 
+test9  = (env1, one :*: zero, "1 * 0 = ")
+test10 = (env1, one :+: (zero :*: two), "1 * 0 = ")
+test11 = (env1, two :*: (one :+: zero), "2 * (1 + 0) = ")
+test12 = (env1, two :*: (two :*: (zero :*: zero)), "2 * (2 *(0 * 0)) = ")
+test13 = (env1, two :*: (two :+: (zero :*: zero)), "2 + (0 * 0) = ")
+test14 = (env2, (NVar zz), "zz = ")
+test15 = (env3, (NVar opo), "opo = ")
+test16 = (env4, (NVar ftt), "ftt = ")
+test17 = (env2, (two :+: (NVar zz)), "2 + zz = ")
+test18 = (env2, (two :*: (NVar opo)), "2 * opo = ")
 
-tests = [test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16]
+tests = [test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12, test13, test14, test15, test16, test17, test18]
 
 run_tests :: [Test] -> IO ()
 run_tests ts = run_t 1 ts
   where
     run_t n []     = return ()
-    run_t n ((env,b):ts) = do
+    run_t n ((env,b,text):ts) = do
       putStr ("test " ++ show n ++ ":\n")
+      putStrLn (show text)
       start_nat env b
       putStr "\n"
       run_t (n+1) ts
